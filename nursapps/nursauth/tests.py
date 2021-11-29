da2d0cf229b8b68d2969968371f74e2cdcc4570d
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-11-29 14:58:11 +0100

    1st test

--- nursapps/nursauth/tests.py
+++ nursapps/nursauth/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+"""Tests nursauth module."""
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class TestUserRegistration(TestCase):
+    """Test user registration."""
+
+    def setUp(self):
+        """SetUp."""
+        self.User = get_user_model()
+        self.client = Client()
+        self.inscript_credentials = {
+            "email": "john@doe.com",
+            "username": "john",
+            "password1": "poufpouf",
+            "password2": "poufpouf",
+        }
+        self.login_credentials = {"email": "john@doe.com", "password": "poufpouf"}
+
+    def test_user_registration(self):
+        """test form data.
+
+        The view needs at least:
+        - an email
+        - a username
+        - a password 1 & 2
+
+        If the registration is complete, the page is redirected
+        """
+        response = self.client.post(
+            "/auth/accounts/inscript/", self.inscript_credentials
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(len(self.User.objects.all()), 1)
