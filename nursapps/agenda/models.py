b0e9c57e8861bbcd0057ef519e6cfa5252193717
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-11-30 13:52:49 +0100

    Agrenda models: create Cabinet, AssociateManager, Associate & RequestAssociate classes

--- nursapps/agenda/models.py
+++ nursapps/agenda/models.py
@@ -1,3 +1,50 @@
+"""Agenda models module."""
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from nursapps.nursauth.models import User
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Cabinet(models.Model):
+    """Cabinet assoc model."""
+
+    name = models.CharField(max_length=240, unique=True, default=False, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class AssociateManager(models.Manager):
+    """Associate manager."""
+
+    def get_associates(self, cabinet_id):
+        """Get associates."""
+        associate = self.all().filter(cabinet_id=cabinet_id)
+        associates = User.objects.filter(id__in=[i.user_id for i in associate])
+        return associates
+
+
+class Associate(models.Model):
+    """Stores the user and the company they are affiliated with."""
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    cabinet = models.ForeignKey(Cabinet, on_delete=models.CASCADE)
+
+    objects = AssociateManager()
+
+    def __str__(self) -> str:
+        """Str representation."""
+        return f"{self.user} - {self.cabinet}"
+
+
+class RequestAssociate(models.Model):
+    """Request to associate the new user with the owner of the cabinet."""
+
+    sender_id = models.CharField(max_length=10, default=False, blank=True)
+    receiver_id = models.CharField(max_length=10, default=False, blank=True)
+    cabinet_id = models.CharField(max_length=10, default=False, blank=True)
