91644b79bcb5366769576784d0828962957b8f74
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-11-30 13:56:24 +0100

    agenda views: add temporary views

--- nursapps/agenda/views.py
+++ nursapps/agenda/views.py
@@ -1,3 +1,19 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def index(request):
+    """Index view."""
+    # cal = CustomHTMLCal(int(current_year), int(current_month))
+    # return render(request, "pages/home.html", {"cal": cal.formatmonth()})
+    return HttpResponse("Page d'accueil")
+
+
+# @login_required
+def home(request, year, month):
+    """Home calendar view."""
+    #     cal = CustomHTMLCal(year, month)
+    #     return render(request, "pages/home.html", {"cal": cal.formatmonth()})
+    return HttpResponse("Page d'accueil")
