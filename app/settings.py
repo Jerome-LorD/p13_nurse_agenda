98e4386d7a8d83942aabce3ec230b38615169b38
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-12-03 14:49:48 +0100

    settings: rm whitenoise

--- app/settings.py
+++ app/settings.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -162,7 +161,6 @@ STATIC_URL = "/static/"
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # prod
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
