b3e97a0b2fcb74296eced3d654e0d367807529b9
Jerome-LorD <71758882+Jerome-LorD@users.noreply.github.com>
2021-12-04 18:08:44 +0100

    upd

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ if [ "$TRAVIS_BRANCH" != "develop" ]; then
     exit 0;
 fi
 
-git checkout refs/remotes/origin/main
-git merge refs/remotes/origin/develop
+git checkout $TRAVIS_BRANCH
+git merge develop
 git push
